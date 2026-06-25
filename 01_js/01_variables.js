f0c50615cea854c9bc0ba7de282288d709a93abe
Aniket Singh <a.niket.singh72909@gmail.com>
2026-06-25 13:16:38 +0000

    Day_1

--- 01_js/01_variables.js
+++ 01_js/01_variables.js
@@ -1,8 +1,24 @@
 const accountId = 123445
 let accountEmail = "ani@bing.com"
-var accoundPass = "raketlkjsdf"
+var accountPass = "raketlkjsdf"
 accountCity = "Jabalpur"
+let accountState;
 
-accountId=3234
+/*
+  Prefer not to use 'var'
+  because of issue in block scope and functional scope
 
-console.log(accountId);
+*/
+
+
+
+// accountId=3234 // can't be changed because it is const 
+
+accountEmail = "as@gmail.com"
+accountPass = "dfjlkjiou"
+accountCity = "Bhopal"
+
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
