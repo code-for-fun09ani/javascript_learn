026ad7f7b254cafa487529b231144eec12d3b830
Aniket Singh <a.niket.singh72909@gmail.com>
2026-06-26 14:45:48 +0000

    Day_2

---
+++ 01_js/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "23abc"
+
+console.log(typeof score)
+
+let scoreInNum = Number(score)
+
+console.log(scoreInNum)
+
+console.log(typeof scoreInNum)
+
+/*
+
+Number conversion converts following into 
+true=> 1, false=>0
+23ddf=> Nan
+"22"=> 22
+
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+/*
+
+1=>true; 0=>false
+""=>false ; "aniket"=> true
+null => false ; undefined => false
+
+*/
+
