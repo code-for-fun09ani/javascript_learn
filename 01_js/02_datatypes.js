b73f6fd37efe80912b59ffc0bb64b69bcb42eaa2
Aniket Singh <a.niket.singh72909@gmail.com>
2026-06-26 06:05:42 +0000

    Day_2

---
+++ 01_js/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //it treats all JS code as newer version
+
+// alert( 3+5 ) // We are using nodejs not browser 
+
+console.log(3+6, "Aniket")
+
+
+let name = "Nat Stark"   //string datatype
+let age  = 29            // number datatype
+let isLoggedIn = false   // boolean datatype
+let state = undefined   // type is undefined
+
+// console.table([name,age,isLoggedIn,state,typeof(state)])
+
+// number ==> 2 to power 53
+// bigint ==>
+// string ==> " "
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> 
+// symbol ==> for uniqueness 
+// object
+
+
+console.log(typeof("Aniket"))
+console.log(typeof null)  //type is object
