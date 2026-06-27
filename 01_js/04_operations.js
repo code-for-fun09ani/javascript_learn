d85cd89ea0e6d4801789965162babc3491b8faf7
Aniket Singh <a.niket.singh72909@gmail.com>
2026-06-27 06:20:27 +0000

    Day_3

---
+++ 01_js/04_operations.js
@@ -0,0 +1,24 @@
+// console.log(3%2)  // for remainder
+// console.log(2**3) // for power calculation
+
+
+// let str1= "Ani"
+// let str2 = "ket"
+
+// let str3 = str1+str2
+
+// console.log(str3)
+
+// console.log(2+2+"3")
+
+// let x = "2" + 3 + 53
+// let y = 2 + 35 + "34"
+
+// console.log(y)
+
+let gameCounter = 20
+
+++ gameCounter
+
+console.log(gameCounter);
+
